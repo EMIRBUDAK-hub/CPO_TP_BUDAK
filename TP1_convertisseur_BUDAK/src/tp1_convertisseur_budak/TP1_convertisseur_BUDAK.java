b58cf481ed12322b8c6111d1842526e0401e362f
emir budak <emir.budak@epfedu.fr>
2025-10-22 12:53:00 +0200

    avancer de l'exo2 tp1

---
+++ TP1_convertisseur_BUDAK/src/tp1_convertisseur_budak/TP1_convertisseur_BUDAK.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp1_convertisseur_budak;
+
+import java.util.Scanner;
+
+/**
+ *TP1 DE L'EXO 2
+ * @author emirb
+ */
+public class TP1_convertisseur_BUDAK {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Entre un premier nombre:" );
+        double degres = sc.nextDouble();  
+        double kelvin=0.0;
+        kelvin=273+degres;
+        System.out.println("La temperature en kelvin vaut "+kelvin);
+               
+        
+        
+        
+        
+        
+    }
+    
+}
