cfa407ddcee2971a17452ed8ad11ab153a347612
emir budak <emir.budak@epfedu.fr>
2025-10-22 12:03:07 +0200

    debut tp1

---
+++ TP1_manipNombresInt_BUDAK/src/tp1_manipnombresint_budak/TP1_manipNombresInt_BUDAK.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp1_manipnombresint_budak;
+
+import java.util.Scanner;
+
+/**
+ *TP1 de BUDAK EMIR
+ * @author emirb
+ */
+public class TP1_manipNombresInt_BUDAK {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+        System.out.println("Entre un premier nombre:" );
+        int nb1 = sc.nextInt();  
+        System.out.println("Entre un deuxieme nombre:" );
+        int nb2= sc.nextInt();
+    }
+    
+}
