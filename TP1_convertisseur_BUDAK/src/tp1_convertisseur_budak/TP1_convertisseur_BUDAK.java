6c6bc13bd54f7594632011404b20497ef71becb7
emir budak <emir.budak@epfedu.fr>
2025-10-24 12:58:35 +0200

    tp1 convertisseur  manque plus que la fin du 5

--- TP1_convertisseur_BUDAK/src/tp1_convertisseur_budak/TP1_convertisseur_BUDAK.java
+++ TP1_convertisseur_BUDAK/src/tp1_convertisseur_budak/TP1_convertisseur_BUDAK.java
@@ -17,17 +17,67 @@ public class TP1_convertisseur_BUDAK {
      */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Entre un premier nombre:" );
-        double degres = sc.nextDouble();  
-        double kelvin=0.0;
-        kelvin=273+degres;
-        System.out.println("La temperature en kelvin vaut "+kelvin);
-               
+        System.out.println("Bonjour, Saissez une valeur :" );
+        double temp = sc.nextDouble();
+        System.out.println("Saisissez la conversion que vous souhaiter effectuer : ");
+        int nb = sc.nextInt();
+        
+        if (nb==1);
+        double tf= CelsiusVersKelvin(temp);
+        System.out.println(temp + " degres Celsius est egale a " + tf + "kelvin");
+        
+        if (nb==2);
+        double tf1= KelvinVersCelsius(temp); 
+        System.out.println(temp + "Kelvin est egal a " + tf1 + "degres Celsius");
+        
+        if (nb==3);
+        double tf2=FarenheitVersCelsius(temp);
+        System.out.println(temp + "farenheit est egale a " + tf + "degres Celsius");
+        
+        if (nb==4);
+        double tf3=CelsiusVersFarenheit(temp);
+        System.out.println(temp + "degres celsius est egale a " + tf3 + "Farenheit");
+        
+        if (nb==5);
+        double tf4=KelvinVersFarenheit(temp);
+        System.out.print
+                
         
         
         
+            
         
+        double tempK = CelsiusVersKelvin(temp);  // appel de la fonction
+        System.out.println("La temperature en Kelvin vaut : " + tempK);
         
     }
+    public static double CelsiusVersKelvin(double tCelsius) {
+    double kelvin = tCelsius + 273.00;  
+    return kelvin; 
+    }
     
+    public static double KelvinVersCelsius(double tKelvin){
+    double tCelsius = tKelvin -273.00;
+    return tCelsius;   
+    }
+    
+    public static double FarenheitVersCelsius(double tFarenheit){
+    double tCelsius = (tFarenheit-32.00)*(5/9);
+    return tCelsius;
+    }
+    
+    public static double CelsiusVersFarenheit(double tCelsius){
+    double tFarenheit = tCelsius*(9/5) + 32.00;
+    return tFarenheit;
+    }
+    
+    public static double KelvinVersFarenheit(double tKelvin){
+    double tFarenheit = (tKelvin-273.00)*(9/5) + 32.00;
+    return tFarenheit;   
+    }
+    
+    public static double FarenheitVersKelvin(double tFarenheit){
+    double tKelvin = (tFarenheit-32.00)*(5/9)+273.00;
+    return tKelvin; 
+    }
 }
