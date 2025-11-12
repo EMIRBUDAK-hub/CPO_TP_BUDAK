55164d6f14e5f29c5b52c31ca539824013927dde
emir budak <emir.budak@epfedu.fr>
2025-11-12 11:07:36 +0100

    tp3 exo1 classe arme fait

---
+++ TP3 EXO1/src/tp3/exo1/TP3EXO1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp3.exo1;
+
+/**
+ *tp3 ex1
+ * @author emirb
+ */
+public class TP3EXO1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
